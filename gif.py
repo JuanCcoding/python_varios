b7ea3994006cd45024c6bdd150e0995dd6b12a1a
JuanCcoding <juan_carlos_calvo@hotmail.com>
2025-01-10 12:10:34 +0100

    añadido clic de pausa y reanudación (proyecto de tomar capturas de un gif)

--- gif.py
+++ gif.py
@@ -2,23 +2,46 @@ import tkinter as tk
 
 root = tk.Tk()
 
-framesNum = 160
 archivo = "logo.gif"
 
-# Lista de todas las imágenes del gif
-frames = [tk.PhotoImage(file=archivo, format='gif -index %i' % i) for i in range(framesNum)]
+# Obtener el número de frames del gif
+frames = []
+i = 0
+while True:
+    try:
+        frame = tk.PhotoImage(file=archivo, format=f'gif -index {i}')
+        frames.append(frame)
+        i += 1
+    except tk.TclError:
+        break
+
+framesNum = len(frames)
+running = True  # Variable para controlar el estado de la animación
 
 def update(ind):
     """ Actualiza la imagen gif """
-    frame = frames[ind]
-    ind += 1
-    if ind == framesNum:
-        ind = 0
-    canvas.create_image(0, 0, image=frame, anchor=tk.NW)
-    root.after(20, update, ind) # Numero que regula la velocidad del gif
+    if running:
+        frame = frames[ind]
+        ind += 1
+        if ind == framesNum:
+            ind = 0
+        canvas.create_image(0, 0, image=frame, anchor=tk.NW)
+        root.after(20, update, ind) # Numero que regula la velocidad del gif
 
-canvas = tk.Canvas(root, width=300, height=100) # Modificar segun el tamaño de la imagen
+def toggle_animation(event):
+    """ Detiene o reanuda la animación al hacer clic """
+    global running
+    running = not running
+    if running:
+        update(0)
 
+canvas = tk.Canvas(root, width=frames[0].width(), height=frames[0].height()) # Ajustar el tamaño del canvas al tamaño de la imagen
 canvas.pack()
+
+# Mantener una referencia a la imagen para evitar que sea recolectada por el garbage collector
+canvas.image = frames[0]
+
+canvas.bind("<Button-1>", toggle_animation)  # Vincular el clic del ratón a la función toggle_animation
+
 root.after(0, update, 0)
 root.mainloop()
